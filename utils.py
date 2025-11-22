8cf02ed6f632363eb627fa86e40fc97a69581ad0
darknagi <narutohindidubbed16@gmail.com>
2025-11-22 12:13:13 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,9 +5,6 @@ import json
 # JSON Beautifier
 # -------------------------------
 def clean_json(data):
-    """
-    Converts raw API JSON into clean readable format
-    """
     try:
         if isinstance(data, (str, bytes)):
             try:
@@ -26,13 +23,16 @@ def validate_input(mode, text):
     text = text.strip().upper()
 
     patterns = {  
-        "mobile": r"^\d{10}$",  
-        "gst": r"^[0-9A-Z]{15}$",  
-        "ifsc": r"^[A-Z]{4}0[A-Z0-9]{6}$",  
-        "pincode": r"^\d{6}$",  
-        "vehicle": r"^[A-Z]{2}\d{2}[A-Z]{1,3}\d{3,4}$", 
-        "imei": r"^\d{15}$"
-    }  
+        "mobile": r"^\d{10}$",
+        "gst": r"^[0-9A-Z]{15}$",
+        "ifsc": r"^[A-Z]{4}0[A-Z0-9]{6}$",
+        "pincode": r"^\d{6}$",
+        "vehicle": r"^[A-Z]{2}\d{2}[A-Z]{1,3}\d{3,4}$",
+        "imei": r"^\d{15}$",
+
+        # ✅ Aadhaar Added (Exact 12 digits)
+        "aadhaar": r"^\d{12}$"
+    }
 
     if mode in patterns and re.fullmatch(patterns[mode], text):  
         return True  
